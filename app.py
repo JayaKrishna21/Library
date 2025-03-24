9294a1a99b794b607c304b8d6921b43934912753
Jaya Krishna G <81529076+JayaKrishna21@users.noreply.github.com>
2025-03-24 05:04:13 +0000

    Intiated app.py

--- app.py
+++ app.py
@@ -13,3 +13,10 @@ def admin():
 
     return user_id, password
 
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+def front_page():
+    return(render_template("create.html"))
